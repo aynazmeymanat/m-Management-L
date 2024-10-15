aae6f887ca484293f8721a73539490451f5aa3a1
aynazmeymanat <aynazmeymanat@gmail.com>
2024-10-15 14:39:19 +0330

    add Libary Class with fields and constructor

---
+++ src/Library.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+    private String name;
+    private List<Book> books;
+    private List<Member> members;
+
+    public Library(String name) {
+        this.name = name;
+        this.books = new ArrayList<>();
+        this.members = new ArrayList<>();
+    }
+}
