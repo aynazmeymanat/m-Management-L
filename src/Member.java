47324a1eb0327ea57fb530a259c42b6aea52ed86
aynazmeymanat <aynazmeymanat@gmail.com>
2024-10-15 14:31:00 +0330

    add getter - setter and other methods of Member Class.

--- src/Member.java
+++ src/Member.java
@@ -23,4 +23,42 @@ public class Member {
         this.name = name;
         this.borrowedBooks = new ArrayList<>();
     }
+
+    public void borrowBook(Book book) {
+        if (borrowedBooks.size() >= 3) {
+            System.out.println("A maximum of 3 books can be borrowed");
+            return;
+        }
+        if (!book.isAvailable()) {
+            System.out.println( book.getTitle() + " not available" );
+            return;
+        }
+        borrowedBooks.add(book);
+        book.setAvailable(false);
+        System.out.println( book.getTitle() + "It was loaned");
+    }
+
+    public void returnBook(Book book) {
+        if (borrowedBooks.remove(book)) {
+            book.setAvailable(true);
+            System.out.println( book.getTitle() + "withdrawn");
+        } else {
+            System.out.println( book.getTitle() + "Not found in the list");
+        }
+    }
+
+    public void printMemberInfo() {
+        System.out.println("Member ID: " + memberId);
+        System.out.println("Name: " + name);
+        System.out.print("Borrowed Books: ");
+        if (borrowedBooks.isEmpty()) {
+            System.out.println("No book has been borrowed");
+        } else {
+            for (Book book : borrowedBooks) {
+                System.out.print(book.getTitle() + " " );
+            }
+        }
+    }
+
+
 }
