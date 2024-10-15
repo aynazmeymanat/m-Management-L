f28f12875bf785ec5a7afe591cd027488e13f8a8
aynazmeymanat <aynazmeymanat@gmail.com>
2024-10-15 14:42:17 +0330

    add methods of Library Class .

--- src/Library.java
+++ src/Library.java
@@ -12,4 +12,37 @@ public class Library {
         this.books = new ArrayList<>();
         this.members = new ArrayList<>();
     }
+
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println( book.getTitle() + " add to library");
+    }
+
+
+    public void addMember(Member member) {
+        members.add(member);
+        System.out.println( member.getName() + " Registered in the library");
+    }
+
+
+    public void borrowBook(Book book, Member member) {
+        member.borrowBook(book);
+    }
+
+    public void returnBook(Book book, Member member) {
+        member.returnBook(book);
+    }
+
+    public void printLibraryInfo() {
+        System.out.println("library " + name);
+        System.out.println("books");
+        for (Book book : books) {
+            book.printBookInfo();
+        }
+        System.out.println("member");
+        for (Member member : members) {
+            member.printMemberInfo();
+
+        }
+    }
 }
